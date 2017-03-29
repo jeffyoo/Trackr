591c1db6b45e3e47b63b3b603dd7fe2066934b84
Jeffrey Yoo <jeffyoo@users.noreply.github.com>
2017-03-29 14:51:37 -0700

    Update user.router.js

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -9,12 +9,4 @@ userRouter.put('/applySignupSettings', userController.applySignupSettings);
 userRouter.put('/updateUserSettings', userController.updateUserSettings);
 userRouter.put('/changedPassword', userController.changedPassword)
 
-//userRouter.put('/:_id', updateUser); // Use this is we include account settings (11/15/16)
-//userRouter.delete('/:_id', userController.deleteUser); //tutorial, we might need this
-
-
-
-//userRouter.post('/register', userController.registerUser); //tutorial, might be same as createUser
-//userRouter.post('/authenticate', userController.authenticateUser); //tutorial, might be same as loginUser
-
 module.exports = userRouter;
